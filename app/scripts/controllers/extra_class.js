c571e11eece63a73f834cd534dd9d5c5a723c129
Sanjay Thomas Alanoly <me@sanjayalanoly.com>
2016-05-26 15:19:20 +0530

    Formatting

--- app/scripts/controllers/extra_class.js
+++ app/scripts/controllers/extra_class.js
@@ -13,50 +13,51 @@ angular.module('mobileApp')
 	var user = user_service.getUser();
 	var user_id = user.user_id;
 
+	//Below code is to convert POST to GET request
 
-	// Use x-www-form-urlencoded Content-Type
-	$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+		// Use x-www-form-urlencoded Content-Type
+		$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
 
-	/**
-	* The workhorse; converts an object to x-www-form-urlencoded serialization.
-	* @param {Object} obj
-	* @return {String}
-	*/ 
-	var param = function(obj) {
-		var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
-		  
-		for(name in obj) {
-		  value = obj[name];
-			
-		  if(value instanceof Array) {
-			for(i=0; i<value.length; ++i) {
-			  subValue = value[i];
-			  fullSubName = name + '[' + i + ']';
-			  innerObj = {};
-			  innerObj[fullSubName] = subValue;
-			  query += param(innerObj) + '&';
+		/**
+		* The workhorse; converts an object to x-www-form-urlencoded serialization.
+		* @param {Object} obj
+		* @return {String}
+		*/
+		var param = function(obj) {
+			var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+
+			for(name in obj) {
+			  value = obj[name];
+
+			  if(value instanceof Array) {
+				for(i=0; i<value.length; ++i) {
+				  subValue = value[i];
+				  fullSubName = name + '[' + i + ']';
+				  innerObj = {};
+				  innerObj[fullSubName] = subValue;
+				  query += param(innerObj) + '&';
+				}
+			  }
+			  else if(value instanceof Object) {
+				for(subName in value) {
+				  subValue = value[subName];
+				  fullSubName = name + '[' + subName + ']';
+				  innerObj = {};
+				  innerObj[fullSubName] = subValue;
+				  query += param(innerObj) + '&';
+				}
+			  }
+			  else if(value !== undefined && value !== null)
+				query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
 			}
-		  }
-		  else if(value instanceof Object) {
-			for(subName in value) {
-			  subValue = value[subName];
-			  fullSubName = name + '[' + subName + ']';
-			  innerObj = {};
-			  innerObj[fullSubName] = subValue;
-			  query += param(innerObj) + '&';
-			}
-		  }
-		  else if(value !== undefined && value !== null)
-			query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
-		}
-		  
-		return query.length ? query.substr(0, query.length - 1) : query;
-	};
 
-	// Override $http service's default transformRequest
-	$http.defaults.transformRequest = [function(data) {
-		return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
-	}];
+			return query.length ? query.substr(0, query.length - 1) : query;
+		};
+
+		// Override $http service's default transformRequest
+		$http.defaults.transformRequest = [function(data) {
+			return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+		}];
 
 	ExtraClassCtrl.load = function() {
 		loading();
