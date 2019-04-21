837527e9a5847eff33812995e2a548ebceb19800
Mohit Gupta <mohitgupta1918@gmail.com>
2019-04-21 19:13:37 +0530

    FCM notification fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,45 @@
+// Import and configure the Firebase SDK
+// These scripts are made available when the app is served or deployed on Firebase Hosting
+// If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
+importScripts('https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js');
+
+//Import and configure Workbox base SW
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js');
+
+
+
+// Firebase Messaging SW
+
+console.log("firebase SW initiate")
+var config = {
+  apiKey: "AIzaSyBZ278HcsmVncMN7M4XThCnQfw-h72vqpA",
+  authDomain: "upma-80899.firebaseapp.com",
+  databaseURL: "https://upma-80899.firebaseio.com",
+  projectId: "upma-80899",
+  storageBucket: "upma-80899.appspot.com",
+  messagingSenderId: "440196037821"
+};
+firebase.initializeApp(config);
+
+
+var messaging = firebase.messaging();
+
+// [START background_handler]
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  var notificationTitle = 'Background Message Title';
+  var notificationOptions = {
+    body: 'Background Message body.',
+    icon: './images/icons/icon-192x192.png' 
+  };
+
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
+// [END background_handler]
+
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
